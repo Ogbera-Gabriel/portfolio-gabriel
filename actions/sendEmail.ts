9859403949f64acb698d03bc675b40ae18a7877e
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2024-04-14 15:24:21 +0100

    contact done

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,2 +1,46 @@
 'use server'
 
+import React from 'react';
+import ContactFormEmail from '@/email/contact-form-email';
+import { getErrorMessage, validateString } from '@/lib/utils';
+import { Resend } from 'resend'
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (formdata: FormData) => {
+    const message = formdata.get('message')
+    const senderEmail = formdata.get('senderEmail')
+
+    if (!validateString(senderEmail, 500)) {
+        return {
+          error: "Invalid sender email",
+        };
+      }
+      if (!validateString(message, 5000)) {
+        return {
+          error: "Invalid message",
+        };
+      }
+
+    let data;
+     try {
+        data= await resend.emails.send({
+            from: 'Contact Form <onboarding@resend.dev>',
+            to: 'ogberagabriel234@gmail.com',
+            subject: 'Message from contact form',
+            reply_to: senderEmail,
+            react: React.createElement(ContactFormEmail, {
+                message: message,
+                senderEmail: senderEmail,
+            })
+        })
+     } catch (error: unknown) {
+        return {
+            error: getErrorMessage(error)
+        }
+     }
+   
+    return {
+        data,
+    };
+};
--- components/contact.tsx
+++ components/contact.tsx
@@ -1,61 +1,72 @@
-'use client'
+"use client";
 
-import React from 'react'
-import SectionHeading from './section-heading'
-import { useSectionInView } from '@/lib/hooks'
-import { motion } from 'framer-motion'
+import React from "react";
+import SectionHeading from "./section-heading";
+import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+import { sendEmail } from "@/actions/sendEmail";
+import SubmitBtn from "./submit-btn";
+import toast from "react-hot-toast";
 
 export default function Contact() {
-    const { ref } = useSectionInView("Contact")
+  const { ref } = useSectionInView("Contact");
   return (
-    <motion.section 
-    id='contact'
-    ref={ref}
-    className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center'
-    initial={{
-        opacity: 0
-    }}
-    whileInView={{
-        opacity: 1
-    }}
-    transition={{
-        duration: 1
-    }}
-    viewport={{
-        once: true
-    }}
+    <motion.section
+      id="contact"
+      ref={ref}
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
     >
-       <SectionHeading>Contact Me</SectionHeading>
-       <p className='text-gray-700 -mt-6'>Please contact me directly at <a className='underline' href='mailto:ogberagabriel234@gmail.com'>ogberagabriel234@gmail.com</a>{" "} or through this form</p>
+      <SectionHeading>Contact Me</SectionHeading>
+      <p className="text-gray-700 -mt-6">
+        Please contact me directly at{" "}
+        <a className="underline" href="mailto:ogberagabriel234@gmail.com">
+          ogberagabriel234@gmail.com
+        </a>{" "}
+        or through this form
+      </p>
 
-       <form 
-       className='mt-10 flex flex-col'
-      //  action={async (formData) => {
-      //     const { data, error} = await sendEmail(formData)
+      <form
+        className="mt-10 flex flex-col"
+        action={async (formData) => {
+         const {data, error} =  await sendEmail(formData);
 
-      //     if (error) {
-      //       toast.error(error);
-      //       return
-      //     }
-      //  }}
-       >
-         <input 
-         className='h-14 px-4 rounded-lg borderBlack transition-all'
-         name='senderEmail'
-         type='email'
-         required
-         maxLength={500}
-         placeholder='Your email'
-         />
-         <textarea 
-         className='h-52 my-3 rounded-lg borderBlack p-4 transition-all'
-         name='message'
-         placeholder='Your message'
-         required
-         maxLength={5000}
-         />
-         <button type='submit'>Submit </button>
-       </form>
+         if (error) {
+          toast.error(error)
+          return;
+         }
+
+         toast.success('Email sent successfully!');
+        }}
+      >
+        <input
+          className="h-14 px-4 rounded-lg borderBlack transition-all"
+          name="senderEmail"
+          type="email"
+          required
+          maxLength={500}
+          placeholder="Your email"
+        />
+        <textarea
+          className="h-52 my-3 rounded-lg borderBlack p-4 transition-all"
+          name="message"
+          placeholder="Your message"
+          required
+          maxLength={5000}
+        />
+        <SubmitBtn />
+      </form>
     </motion.section>
-  )
+  );
 }
